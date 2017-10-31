d7b5cdc445c2236ce96ebb578082574d77df88b7
Marc <marc.venturini@gmail.com>
2017-10-31 21:57:33 +0800

    Added key debouncing

--- firmware/matrix.c
+++ firmware/matrix.c
@@ -3,7 +3,13 @@
 #include "matrix_right.h"
 #include "matrix_left.h"
 
+// Key states maintained less than N cycles are ignored
+// To define N set the N least significant bits of DEBOUNCE_MASK
+#define DEBOUNCE_MASK 0b00011111 // N = 5
+
 matrix_t matrix;
+
+static matrix_t history;
 
 void matrix_init()
 {
@@ -16,7 +22,24 @@ static void update_rows(uint8_t rows, uint8_t column)
 {
     for (uint8_t row=0; row<MATRIX_ROWS; row++)
     {
-        matrix.array[row][column] = (rows & (1 << row)) != 0;
+        // Accumulate the history: left shift the history and set
+        // the least significant bit according to the current state
+        uint8_t pressed = (rows & (1 << row)) != 0;
+        history.array[row][column] <<= 1;
+        history.array[row][column] |= pressed;
+
+        // Check the history during the N past cycles
+        switch (history.array[row][column] & DEBOUNCE_MASK)
+        {
+            // The key was consistently released
+            case 0:
+                matrix.array[row][column] = 0;
+                break;
+            // The key was consistently pressed
+            case DEBOUNCE_MASK:
+                matrix.array[row][column] = 1;
+                break;
+        }
     }
 }
 
