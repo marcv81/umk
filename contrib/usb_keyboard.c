69d5b7117875291eef2b19bc2a381e88baf48da5
Marc <marc.venturini@gmail.com>
2017-12-10 23:09:55 +0800

    Removed unused USB constants

--- contrib/usb_keyboard.c
+++ contrib/usb_keyboard.c
@@ -39,12 +39,7 @@
  **************************************************************************/
 
 #define EP_TYPE_CONTROL                 0x00
-#define EP_TYPE_BULK_IN                 0x81
-#define EP_TYPE_BULK_OUT                0x80
 #define EP_TYPE_INTERRUPT_IN            0xC1
-#define EP_TYPE_INTERRUPT_OUT           0xC0
-#define EP_TYPE_ISOCHRONOUS_IN          0x41
-#define EP_TYPE_ISOCHRONOUS_OUT         0x40
 
 #define EP_SINGLE_BUFFER                0x02
 #define EP_DOUBLE_BUFFER                0x06
@@ -53,8 +48,6 @@
                         ((s) == 32 ? 0x20 :     \
                         ((s) == 16 ? 0x10 :     \
                                      0x00)))
-
-#define MAX_ENDPOINT            4
 
 #define LSB(n) (n & 255)
 #define MSB(n) ((n >> 8) & 255)
@@ -83,14 +76,10 @@
 
 // standard control endpoint request types
 #define GET_STATUS                      0
-#define CLEAR_FEATURE                   1
-#define SET_FEATURE                     3
 #define SET_ADDRESS                     5
 #define GET_DESCRIPTOR                  6
 #define GET_CONFIGURATION               8
 #define SET_CONFIGURATION               9
-#define GET_INTERFACE                   10
-#define SET_INTERFACE                   11
 // HID (human interface device)
 #define HID_GET_REPORT                  1
 #define HID_GET_IDLE                    2
@@ -98,10 +87,6 @@
 #define HID_SET_REPORT                  9
 #define HID_SET_IDLE                    10
 #define HID_SET_PROTOCOL                11
-// CDC (communication class device)
-#define CDC_SET_LINE_CODING             0x20
-#define CDC_GET_LINE_CODING             0x21
-#define CDC_SET_CONTROL_LINE_STATE      0x22
 
 /**************************************************************************
  *
