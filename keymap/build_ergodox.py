1558cbdbe60ad106da82ae12619dac449f2f11a2
Marc <marc.venturini@gmail.com>
2018-01-19 18:38:45 +0800

    Improved the keymap
    F1..F9 now share keys with 1..9, which is easier to remember.
    [ and ] are now on the same hand, "[]" and "{}" are easier to type.

--- keymap/build_ergodox.py
+++ keymap/build_ergodox.py
@@ -3,9 +3,9 @@ import keymap
 layer1 = """
 
  Esc    1     2     3     4     5    ---   ---    6     7     8     9     0     =
-  '     Q     W     E     R     T     [     ]     Y     U     I     O     P     -
+  '     Q     W     E     R     T     [     \     Y     U     I     O     P     -
 Ctrl    A     S     D     F     G    xxx   xxx    H     J     K     L     ;    Ctrl
-Shift   Z     X     C     V     B     `     \     N     M     ,     .     /   Shift
+Shift   Z     X     C     V     B     ]     `     N     M     ,     .     /   Shift
 Layer Super  Alt   ---   ---   xxx   xxx   xxx   xxx   ---   ---   Alt  Super Layer
  xxx   ---   Tab  Space  Mute  Vol-  Vol+  ---   ---   ---  BckSp Enter  ---   xxx
 
@@ -13,8 +13,8 @@ Layer Super  Alt   ---   ---   xxx   xxx   xxx   xxx   ---   ---   Alt  Super La
 
 layer2 = """
 
- ---   F1    F2    F3    F4    F5    F6     F7    F8    F9   F10   F11   F12   ---
- ---   ---   ---  PgUp   ---   ---   ---   ---   ---   ---    Up   ---   ---   ---
+ ---   F1    F2    F3    F4    F5    ---   ---    F6    F7    F8    F9   F10   F11
+ ---   ---   ---  PgUp   ---   ---   ---   ---   ---   ---    Up   ---   ---   F12
 Ctrl   ---  Home  PgDn   End   ---   xxx   xxx   ---   Left  Down Right  ---   Ctrl
 Shift  ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---  Shift
 Layer Super  Alt   ---   ---   xxx   xxx   xxx   xxx   ---   ---   Alt  Super Layer
--- keymap/build_xd75.py
+++ keymap/build_xd75.py
@@ -4,16 +4,16 @@ layer1 = """
 
  Esc    1     2     3     4     5   Mute   Vol-  Vol+   6     7     8     9     0     =
   '     Q     W     E     R     T    ---   ---   ---    Y     U     I     O     P     -
-Ctrl    A     S     D     F     G     [    ---    ]     H     J     K     L     ;    Ctrl
-Shift   Z     X     C     V     B     `    ---    \     N     M     ,     .     /   Shift
+Ctrl    A     S     D     F     G     [    ---    \     H     J     K     L     ;    Ctrl
+Shift   Z     X     C     V     B     ]    ---    `     N     M     ,     .     /   Shift
 Layer Super  Alt   ---   ---  Space  Tab   ---   Enter BckSp ---   ---   Alt  Super Layer
 
 """
 
 layer2 = """
 
- ---   F1    F2    F3    F4    F5    F6    ---    F7    F8    F9   F10   F11   F12   ---
- ---   ---   ---  PgUp   ---   ---   ---   ---   ---   ---   ---    Up   ---   ---   ---
+ ---   F1    F2    F3    F4    F5    ---   ---   ---    F6    F7    F8    F9   F10   F11
+ ---   ---   ---  PgUp   ---   ---   ---   ---   ---   ---   ---    Up   ---   ---   F12
 Ctrl   ---  Home  PgDn   End   ---   ---   ---   ---   ---   Left  Down Right  ---   Ctrl
 Shift  ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---  Shift
 Layer Super  Alt   ---   ---   ---   ---   ---   ---   ---   ---   ---   Alt  Super Layer
