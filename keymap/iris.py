6d300f48fdc7f599a4e64fd8237baa56d5925248
Marc <marc.venturini@gmail.com>
2019-09-17 22:43:21 +0800

    Updated keymaps

--- keymap/iris.py
+++ keymap/iris.py
@@ -15,18 +15,18 @@ layer0 = """
  Esc    1     2     3     4     5                       6     7     8     9     0     =
   '     Q     W     E     R     T                       Y     U     I     O     P     -
 LCtrl   A     S     D     F     G                       H     J     K     L     ;   RCtrl
-LShft   Z     X     C     V     B   LSup         RAlt   N     M     ,     .     /   RShft
-                        Layer Space  Tab        Enter BckSp Layer
+LShft   Z     X     C     V     B   LAlt         RAlt   N     M     ,     .     /   RShft
+                        Layer Space LSup         RSup BckSp Layer
 
 """
 
 layer1 = """
 
  ---   F1    F2    F3    F4    F5                       F6    F7    F8    F9   F10   F11
- ---   ---    `   PgUp    \    ---                     ---    [     Up    ]    ---   F12
-LCtrl  ---  Home  PgDn   End   ---                     ---   Left  Down Right  ---  RCtrl
-LShft  ---  Mute  Vol-   Vol+  ---  LSup         RAlt  ---   ---   ---   ---   ---  RShft
-                        Layer  ---   ---         ---   ---  Layer
+ ---   ---   ---    `     \    ---                     PgUp  Home   Up   End   ---   F12
+LCtrl  ---   ---    [     ]    ---                     PgDn  Left  Down Right  ---  RCtrl
+LShft  ---  Mute   Vol-  Vol+  ---  LAlt         RAlt  ---   ---   ---   ---   ---  RShft
+                        Layer  Tab  LSup         RSup Enter Layer
 
 """
 
--- keymap/xd75.py
+++ keymap/xd75.py
@@ -12,20 +12,20 @@ mapping = """
 
 layer0 = """
 
- Esc    1     2     3     4     5    F10   F11   F12    6     7     8     9     0     =
-  '     Q     W     E     R     T    F7    F8    F9     Y     U     I     O     P     -
-LCtrl   A     S     D     F     G    F4    F5    F6     H     J     K     L     ;   RCtrl
-LShft   Z     X     C     V     B    F1    F2    F3     N     M     ,     .     /   RShft
-Layer LSup  LAlt  Space       Space  Mute  Vol-  Vol+ BckSp       BckSp  RAlt  RSup Layer
+ Esc    1     2     3     4     5    ---   ---   ---    6     7     8     9     0     =
+  '     Q     W     E     R     T    ---   ---   ---    Y     U     I     O     P     -
+LCtrl   A     S     D     F     G    ---   ---   ---    H     J     K     L     ;   RCtrl
+LShft   Z     X     C     V     B    ---   ---   ---    N     M     ,     .     /   RShft
+Layer LSup  LAlt  Space       Space  ---   ---   ---  BckSp       BckSp  RAlt  RSup Layer
 
 """
 
 layer1 = """
 
- ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---
- ---   ---    `   PgUp    \    ---   ---   ---   ---   ---    [     Up    ]    ---   ---
-LCtrl  ---  Home  PgDn   End   ---   ---   ---   ---   ---   Left  Down Right  ---  RCtrl
-LShft  ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---  RShft
+ ---   F1    F2    F3    F4    F5    ---   ---   ---    F6    F7    F8    F9   F10   F11
+ ---   ---   ---    `     \    ---   ---   ---   ---   PgUp  Home   Up   End   ---   F12
+LCtrl  ---   ---    [     ]    ---   ---   ---   ---   PgDn  Left  Down Right  ---  RCtrl
+LShft  ---  Mute   Vol-  Vol+  ---   ---   ---   ---   ---   ---   ---   ---   ---  RShft
 Layer LSup  LAlt   Tab         Tab   ---   ---   ---  Enter       Enter  RAlt  RSup Layer
 
 """
