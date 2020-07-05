f1200b826bd591ef1f29414d632cb611c20e9fc8
Marc Venturini <marc.venturini@gmail.com>
2020-07-05 20:31:04 +0800

    Moved the XD75 3u spacebars

--- keymap/xd75.py
+++ keymap/xd75.py
@@ -6,7 +6,7 @@ mapping = """
  1,0   1,1   1,2   1,3   1,4   1,5   1,6   1,7   1,8   1,9   1,10  1,11  1,12  1,13  1,14
  2,0   2,1   2,2   2,3   2,4   2,5   2,6   2,7   2,8   2,9   2,10  2,11  2,12  2,13  2,14
  3,0   3,1   3,2   3,3   3,4   3,5   3,6   3,7   3,8   3,9   3,10  3,11  3,12  3,13  3,14
- 4,0   4,1   4,2   4,3         4,5   4,6   4,7   4,8   4,9         4,11  4,12  4,13  4,14
+ 4,0   4,1   4,2   4,3   4,4         4,6   4,7   4,8         4,10  4,11  4,12  4,13  4,14
 
 """
 
@@ -16,7 +16,7 @@ layer0 = """
   '     Q     W     E     R     T    ---   ---   ---    Y     U     I     O     P     -
 LCtrl   A     S     D     F     G    ---   ---   ---    H     J     K     L     ;   RCtrl
 LShft   Z     X     C     V     B    ---   ---   ---    N     M     ,     .     /   RShft
-Layer LSup  LAlt  Space       Space  ---   ---   ---  BckSp       BckSp  RAlt  RSup Layer
+Layer LSup  LAlt  Layer Space       Space  ---  BckSp       BckSp Layer RAlt  RSup  Layer
 
 """
 
@@ -26,7 +26,7 @@ layer1 = """
  ---   ---   ---    `     \    ---   ---   ---   ---   PgUp  Home   Up   End   ---   F12
 LCtrl  ---   ---    [     ]    ---   ---   ---   ---   PgDn  Left  Down Right  ---  RCtrl
 LShft  ---  Mute   Vol-  Vol+  ---   ---   ---   ---   ---   ---   ---   ---   ---  RShft
-Layer LSup  LAlt   Tab         Tab   ---   ---   ---  Enter       Enter  RAlt  RSup Layer
+Layer LSup  LAlt  Layer  Tab         Tab   ---  Enter       Enter Layer RAlt  RSup  Layer
 
 """
 
